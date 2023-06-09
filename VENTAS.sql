53987923839e833bcb2fe31569d48372d25bd039
Palermukii <109637082+Palermukii@users.noreply.github.com>
2023-06-09 17:52:27 -0300

    Update 2 VENTAS.sql

--- VENTAS.sql
+++ VENTAS.sql
@@ -16,7 +16,7 @@
         
     );
     CREATE TABLE PEDIDOS (
-        Id_pedido INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        Id_pedido INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
         Fecha DATE NOT NULL,
         Cantidad INT(3) NOT NULL,
         Id_Cliente INT(3) NOT NULL,
@@ -28,7 +28,8 @@
     VALUES ('Jano', 'Fuertes', '1990-07-08', 1123331123),
     ('Tomas', 'Fuertes', '1990-12-09', 1124671113),
     ('Marrabello', 'Nagito', '2007-12-09', 1124671443),
-    ('Maxmiliano', 'Fuertes', '1390-12-09', 1124671653)
+    ('Maxmiliano', 'Fuertes', '1390-12-09', 1124671653),
+    ('Alcoholico', 'Montesano', '1390-12-09', 1133371653)
     ;
     INSERT INTO PRODUCTOS (Nombre, Descrip, Precio)
      VALUES ('Teclado', 'RGB', 150),
@@ -37,5 +38,6 @@
       ('Placa de Video', '8GB', 550)
     ;
         INSERT INTO PEDIDOS (Fecha, Cantidad, Id_Cliente, Id_Prod)
-     VALUES ('2023-06-09', 4, '1', '3')
+     VALUES ('2023-06-09', 4, '1', '3'),
+      ('2023-06-03', 2, '5', '1')
     ;
